8e97c5128d1e568468331fff05b70c9402da0e0b
Sohel Khan <sohel.khan@indeses.com>
2026-01-28 16:43:47 +0530

    Added code plugin in this

--- frappe_tinymce/public/js/frappe_tinymce.js
+++ frappe_tinymce/public/js/frappe_tinymce.js
@@ -34,10 +34,10 @@ frappe.ui.form.ControlTextEditor = class ControlTextEditor extends OriginalContr
             target: this.$editor_container[0],
             height: 450,
 
-            toolbar: 'undo redo | bold italic underline strikethrough | fontfamily fontsize blocks | alignleft aligncenter alignright alignjustify | outdent indent |  numlist bullist checklist | forecolor backcolor casechange permanentpen formatpainter removeformat | pagebreak | charmap emoticons | fullscreen  preview save print | insertfile image media pageembed template link anchor codesample | a11ycheck ltr rtl | showcomments addcomment | footnotes | mergetags',
+            toolbar: 'undo redo | code | bold italic underline strikethrough | fontfamily fontsize blocks | alignleft aligncenter alignright alignjustify | outdent indent |  numlist bullist checklist | forecolor backcolor casechange permanentpen formatpainter removeformat | pagebreak | charmap emoticons | fullscreen  preview save print | insertfile image media pageembed template link anchor codesample | a11ycheck ltr rtl | showcomments addcomment | footnotes | mergetags',
             font_size_formats: '10px 11px 12px 14px 15px 16px 18px 24px 36px',
             plugins: [
-              'autoresize', 'autolink', 'charmap', 'emoticons', 'fullscreen', 'help',
+              'code', 'autoresize', 'autolink', 'charmap', 'emoticons', 'fullscreen', 'help',
               'image', 'link', 'lists', 'searchreplace',
               'table', 'visualblocks', 'visualchars', 'wordcount', 'media', 'anchor'
             ],
